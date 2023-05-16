403aefb63b5a41afdd078187c8799cdfe14c4f6e
maro114510 <atumaro114510@icloud.com>
2023-05-16 13:51:32 +0900

    2023/05/16 13:51:32 erectronの起動を確認

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+const { ensureDir } = require('fs-extra')
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
+
+	win.loadFile( './html/index.html' )
+}
+
+app.whenReady().then(() => {
+	createWindow()
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
+})
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+
+// End of script
