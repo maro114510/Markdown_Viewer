d11093c05e8f6c8cdecbc9804c338ce79908259a
maro114510 <atumaro114510@icloud.com>
2023-05-17 18:19:17 +0900

    2023/05/17 18:19:17 ファイルのエンコードを検知する機能を実装

--- src/index.js
+++ src/index.js
@@ -2,21 +2,36 @@ const { app, dialog } = require('electron')
 
 // Desc: Get file path from user
 const { getFilePath } = require('./modules/get_filepath')
+const { getFileEncoding } = require('./modules/detect_encoding')
 
 // Main deal
 
 app.on('ready', async () => {
-	await handleGetFilePath();
+	const filePath = await handleGetFilePath();
+	const encoding = await getFileEncoding( filePath );
+	console.log( encoding );
 });
 
-async function handleGetFilePath() {
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+})
+
+// functions
+
+async function handleGetFilePath()
+{
 	let counter = 0;
-	while (counter < 5) {
-		try {
+	while (counter < 5)
+	{
+		try
+		{
 			const filePath = await getFilePath( dialog );
-			console.log(filePath);
-			break;
-		} catch (error) {
+			return filePath;
+		}
+		catch( error )
+		{
 			dialog.showErrorBox('Error', error.message);
 			if (counter === 4) {
 				dialog.showErrorBox('Error', 'You have exceeded the maximum number of attempts.');
@@ -27,10 +42,13 @@ async function handleGetFilePath() {
 	}
 }
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-})
+// Helper functions
+
+function handleGetFilePathError( error )
+{
+	dialog.showErrorBox( 'Error', error.message );
+	app.quit();
+}
+
 
 // End of script
--- src/modules/detect_encoding.js
+++ src/modules/detect_encoding.js
@@ -1,9 +1,11 @@
 // Desc: detect file encoding
 
 const fs = require( 'fs' );
+const jschardet = require('jschardet');
 
 
-function getFileEncoding( filePath ) {
+function getFileEncoding( filePath )
+{
 	return new Promise( ( resolve, reject ) => {
 		fs.readFile( filePath, ( err, data ) => {
 			if( err )
@@ -12,18 +14,26 @@ function getFileEncoding( filePath ) {
 			}
 			else
 			{
-				const encoding = detectFileEncoding( data );
+				const encoding = detectEncoding( data );
 				resolve( encoding );
 			}
 		});
 	});
 }
 
-// Helper functions
-function detectFileEncoding( data ) {
-	// Implement the logic to detect file encoding
-	// Return the detected encoding
+function detectEncoding( data )
+{
+	try
+	{
+		const encoding = jschardet.detect( data ).encoding;
+		return encoding;
+	}
+	catch( error )
+	{
+		throw error;
+	}
 }
+
 
 module.exports = { getFileEncoding };
 
