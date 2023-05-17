4463570eec5d7396a47f56899f269b1a126e0f2f
maro114510 <atumaro114510@icloud.com>
2023-05-17 15:52:36 +0900

    2023/05/17 15:52:36 Save Work

---
+++ src/modules/get_filepath.js
@@ -0,0 +1,27 @@
+// Desc: Get file path from user
+
+function getFilePath( app, dialog ) {
+	return new Promise( ( resolve, reject ) => {
+		app.on('ready', () => {
+			dialog
+			.showOpenDialog({
+				properties: ['openFile']
+			})
+			.then(result => {
+				if (!result.canceled) {
+					const filePath = result.filePaths[0]
+					resolve( filePath );
+				}
+			})
+			.catch(error => {
+				console.log(error);
+				reject( error );
+			});
+		});
+	});
+}
+
+module.exports = { getFilePath };
+
+
+// End of script
