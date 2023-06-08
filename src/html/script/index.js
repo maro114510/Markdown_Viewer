26e8bdb84259f54b570e63bb5cf14ec8b7416b4f
maro114510 <atumaro114510@icloud.com>
2023-06-08 13:59:38 +0900

    2023/06/08 13:59:37 Fixed page break problem

--- src/html/script/index.js
+++ src/html/script/index.js
@@ -1,70 +1,7 @@
 // This is real renderer process
 
 // DOMの読み込み完了後に実行
-document.addEventListener ("DOMContentLoaded", function () {
-	// .page要素を取得
-	var pages = document.querySelectorAll( ".page" );
-	let buffer = [];
-
-	// 各ページに対して処理を実行
-	pages.forEach( function ( page, index ) {
-		// .page要素の高さを取得
-		let pageHeight = page.offsetHeight;
-		// .page要素内のコンテンツの高さを取得
-		let contentHeight = page.scrollHeight;
-
-		// コンテンツがページから溢れているか判定
-		if( pageHeight + 1 < contentHeight )
-		{
-			console.log(
-				"Page " + ( index + 1 ) + "のコンテンツがページから溢れています。"
-			);
-
-			let overflowingElements = Array.from( page.children ).filter( function ( child ) {
-				return child.offsetTop + child.offsetHeight > pageHeight;
-			});
-
-			// 溢れた要素をバッファリングして削除
-			overflowingElements.forEach( function ( element ) {
-				if( element.offsetTop > pageHeight )
-				{
-					element.parentNode.removeChild( element );
-				}
-				else
-				{
-					// コンテンツの大きさがページ高さを下回るまで繰り返す
-					while( pageHeight < contentHeight )
-					{
-						buffer.push( element.lastChild );
-						element.lastChild.remove();
-						contentHeight = page.scrollHeight;
-					}
-				}
-			});
-
-			// 新規ページを作成
-			var newPage = document.createElement( "div" );
-			newPage.classList.add( "page" );
-
-			// バッファリングした要素を新規で追加したページに追加
-			buffer.forEach( function ( element ) {
-				newPage.appendChild( element );
-			});
-
-			// 作成した新規ページを現在のページの後ろに追加
-			page.parentNode.insertBefore( newPage, page.nextSibling );
-
-			// ページ番号を右下に入れる
-			var pageNumber = document.createElement( "div" );
-			pageNumber.classList.add( "page-number" );
-			pageNumber.textContent = index + 1;
-			page.appendChild( pageNumber );
-
-			// バッファを空にする
-			buffer = [];
-		}
-	});
-
+document.addEventListener( "DOMContentLoaded", function () {
 	const ExportButton = document.getElementById( 'export_pdf' );
 	ExportButton.addEventListener( 'click', function () {
 		// 以下の説明
@@ -72,13 +9,74 @@ document.addEventListener ("DOMContentLoaded", function () {
 		 * @desc: Send data to main process
 		 * @param: channel: string
 		 * @param: data: any
-		 * @return: void
 		 */
-		//現在時刻をデータにする
 		const date = new Date();
 		window.api.sendToMain( 'export_pdf', date );
 	});
 });
+
+window.onload = function () {
+	var buffer = [];
+
+	let pages = document.querySelectorAll( ".page" );
+	pages.forEach( function ( page, index ) {
+		contentHeight = page.scrollHeight;
+		pageHeight = page.clientHeight;
+
+		//オーバーフロー時の処理
+		if( pageHeight < contentHeight )
+		{
+			const overflowElements = getOverflowElements( page );
+
+			overflowElements.forEach( function ( element ) {
+				buffer.push( element );
+				page.removeChild( element );
+			});
+
+			const newPage = document.createElement( "div" );
+			newPage.classList.add( "page" );
+			page.parentNode.insertBefore( newPage, page.nextSibling );
+
+			buffer.forEach( function ( element ) {
+				newPage.appendChild( element );
+			});
+
+			buffer = [];
+		}
+	});
+
+	appPageNumber();
+};
+
+function getOverflowElements( page )
+{
+	let overfloawElements = Array.from( page.children ).filter( function ( element ) {
+		return element.offsetTop + element.clientHeight > page.clientHeight;
+	});
+	return overfloawElements;	
+}
+
+//すべてのロードが終了後に、ページクラスが空白なら削除する
+window.onload = function () {
+	var pages = document.querySelectorAll( ".page" );
+	pages.forEach( function ( page, index ) {
+		if( page.children.length === 0 )
+		{
+			page.parentNode.removeChild( page );
+		}
+	});
+};
+
+function appPageNumber()
+{
+	var pages = document.querySelectorAll( ".page" );
+	pages.forEach( function ( page, index ) {
+		var pageNumber = document.createElement( "div" );
+		pageNumber.classList.add( "page-number" );
+		pageNumber.innerHTML = index + 1;
+		page.appendChild( pageNumber );
+	});
+}
 
 
 
