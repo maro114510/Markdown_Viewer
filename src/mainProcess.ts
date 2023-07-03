986a4a0272394b3e499dbdecfbca59396a9e0625
maro114510 <atumaro114510@icloud.com>
2023-07-03 19:42:26 +0900

    2023/07/03 19:42:25 Get file contents

--- src/mainProcess.ts
+++ src/mainProcess.ts
@@ -8,7 +8,6 @@ const { dialog, ipcMain } = require('electron')
 
 // Desc: Get file path from user
 const { getFileEncoding } = require( './modules/detect_encoding' );
-const { getFileContent } = require( './modules/get_file_content' );
 const { ExportPDF } = require( './modules/export_pdf' );
 //const { ErrorWrapper } = require( './modules/error' );
 const { getDirectory } = require( './modules/get_directory' );
@@ -63,7 +62,7 @@ class MarkdownViewer
 
 	async handleLoad( encoding = "utf8" )
 	{
-		const fileContent = await this.handleGetFileContent( this.watchFilesPath[ 0 ], encoding );
+		const fileContent: string = await this.handleGetFileContent( this.watchFilesPath[ 0 ], encoding );
 		const html = this.handleMarkdown( fileContent );
 		this.handleInsertHTML( html );
 
@@ -111,7 +110,7 @@ class MarkdownViewer
 		try
 		{
 			const fileContent = await getFileContent( filePath, encoding );
-			return fileContent;
+			return fileContent as string;
 		}
 		catch( error )
 		{
--- src/modules/get_file_content.ts
+++ src/modules/get_file_content.ts
@@ -1,11 +1,10 @@
 // Desc: get file content
 
-const fs = require( 'fs' );
 
-function getFileContent( filePath, encoding )
+function getFileContent( filePath: string, encoding: string )
 {
 	return new Promise( ( resolve, reject ) => {
-		fs.readFile( filePath, encoding, ( error, data ) => {
+		fs.readFile( filePath, encoding, ( error: Error, data: string ) => {
 			if ( error ) {
 				reject( error );
 			} else {
