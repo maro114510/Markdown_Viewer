a37ba4b77c2870e01d225dd9dff32482bf304ad6
maro114510 <atumaro114510@icloud.com>
2023-07-10 15:02:07 +0900

    2023/07/10 15:02:06 Scratch test cases

--- src/__test__/get_file_content.test.ts
+++ src/__test__/get_file_content.test.ts
@@ -12,8 +12,6 @@ describe( 'getFileContent', () => {
 		const content = await getFileContent( filePath, encoding );
 
 		expect( content ).toBe( fileContent );
-
-		fs.readFile.mockRestore();
 	});
 });
 
@@ -21,8 +19,7 @@ describe( 'getFileContent', () => {
 
 function returnFileContent()
 {
-	const mdText = `
-# header1
+	const mdText = `# header1
 
 ## header2
 
@@ -31,34 +28,97 @@ function returnFileContent()
 #### header4
 
 * list1
-	* list2
+* list2
 
-	> quote
+> quote
 
-	*italic*
+*italic*
 
-	**bold**
+**bold**
 
-	\`code\`
+\`code\`
 
-	\`\`\`python
+\`\`\`python
+print("hello world")
+
+def func():
 	print("hello world")
-	\`\`\`
 
-	[link](https://google.com)
+class Class:
+	def __init__(self):
+		print("hello world")
+\`\`\`
 
-	![image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)
+[link](https://google.com)
+
+![image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)
 
 | header1 | header2 |
 | ------- | ------- |
 | cell1   | cell2   |
 
-	- [ ] task1
-	- [x] task2
-	- [ ] task3
+- [ ] task1
+- [x] task2
+- [ ] task3
+- [ ] mm
+- [ ] task1
+- [x] task2
+- [ ] task3
+
+
+> quote
+> md
+
+\`\`\`rust:main.rs
+fn main() {
+	println!("hello world");
+}
+\`\`\`
+
+### どcけr
+
+っh
 
 ---
 
-`;
+<div style="page-break-after: always;"></div>
+
+---
+
+# header1
+
+> quote
+> quote
+
+\`\`\`python
+print("hello world")
+\`\`\`
+
+"morimori"
+
+> quote1
+> い
+> い
+
+
+#### mori
+
+docker
+
+\`\`\`dockerfile
+FROM ubuntu:18.04
+
+RUN apt-get update && apt-get install -y \\
+	curl \\
+	git \\
+	python3 \\
+	python3-pip \\
+	python3-dev \\
+	python3-setuptools \\
+	python3-wheel \\
+	&& rm -rf /var/lib/apt/lists/*
+\`\`\`
+
+1. list1`;
 	return mdText;
 }
